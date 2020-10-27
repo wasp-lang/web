f9eb8727859c6845f0691ee981b6886d19166f78
Matija Sosic <matija.sosic@gmail.com>
2020-10-27 16:22:03 +0100

    set base url to root.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Wasp',
   tagline: 'A programming language that understands what a web app is.',
   url: 'https://wasp-lang.github.io',
-  baseUrl: '/web/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
   organizationName: 'wasp-lang', // Usually your GitHub org/user name.
