0d8dc91a02dfc2953076e528ebe6b886cd9ab2fb
Matija Sosic <matija.sosic@gmail.com>
2020-11-05 14:58:41 +0100

    Updated meta title.

--- src/pages/index.js
+++ src/pages/index.js
@@ -357,7 +357,7 @@ function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description={siteConfig.tagline}
     >
       <header className={clsx('hero', styles.heroBanner)}>
         <div className="container">
