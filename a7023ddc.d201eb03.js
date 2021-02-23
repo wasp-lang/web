2726be4c3f8973d06079bf8745ddd4d8ec86e10f
Martin Sosic <sosic.martin@gmail.com>
2021-02-23 21:40:55 +0100

    Deploy website - based on 5b41e8dda3ec23e6f864266ccb34490acb561956

---
+++ a7023ddc.d201eb03.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{77:function(a){a.exports=JSON.parse('{"startup":{"allTagsPath":"/blog/tags","slug":"startup","name":"startup","count":1,"permalink":"/blog/tags/startup"},"wasp":{"allTagsPath":"/blog/tags","slug":"wasp","name":"wasp","count":2,"permalink":"/blog/tags/wasp"}}')}}]);
